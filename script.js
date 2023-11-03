c1ae3e81e8373e2f28552f430e0fa9d1b898b509
leandro castro <leandro.713@hotmail.com>
2023-11-03 11:54:33 -0300

    inserindo a imagem do banner

--- script.js
+++ script.js
@@ -12,11 +12,12 @@ let apostas = ['*****0102', '*****2020',,,,,,,'*****2587',,
               '*****00']
   
 for (let i = 1; i < 101; i++){
-  let newDiv = document.createElement('div');
+  let newDiv = document.createElement('a');
   let newNum = document.createElement('p');
   let newX = document.createElement('p');
   let newFone = document.createElement('p');
   let iStr = String(i);
+  iStr = iStr.slice(-2);
   newNum.innerHTML =  iStr.slice(-2);
   
   newX.textContent = '';
@@ -24,6 +25,9 @@ for (let i = 1; i < 101; i++){
   newDiv.appendChild(newNum);
   newDiv.appendChild(newX);
   newDiv.appendChild(newFone);
+  console.log(newDiv)
+  newDiv.setAttribute('href', 'https://wa.me//5551998497549?text=Tenho%20interesse%20em%20comprar%20o%20número%20'+iStr+'%20da%20rifa.')
+  newDiv.setAttribute('target', '_blank')
   newFone.className = 'fone';
   newDiv.className = 'circle';
   newNum.className = 'num';
@@ -36,7 +40,7 @@ for (let i = 1; i < 101; i++){
     newDiv.classList.remove('vermelho')
     newDiv.classList.add('verde')
   };
-  newDiv.addEventListener('onClick', ()=> ({ 'https://wa.me/5551998497549/?text=textourl e insira seu número de telefone' } ))
+  // newDiv.addEventListener('onClick', ()=> ({ 'https://wa.me/5551998497549/?text=textourl e insira seu número de telefone' } ))
   dezenasContainer.className = 'numbers-container';
   dezenasContainer.append(newDiv);
 } 
